4726df9a7cb838a2ed80cc0a12d5b74a07168a51
MoonRunnerGames <declan.mckenna.1994@gmail.com>
2026-05-26 17:04:16 +0100

    add outputs for github MID and FIC

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -12,3 +12,13 @@ output "tfstate_container_name" {
   description = "Blob container used for Terraform remote state."
   value       = azurerm_storage_container.tfstate.name
 }
+
+output "github_actions_client_id" {
+  value       = azurerm_user_assigned_identity.github_actions_mid.client_id
+  description = "Client ID used by GitHub Actions for Azure OIDC login."
+}
+
+output "github_actions_principal_id" {
+  value       = azurerm_user_assigned_identity.github_actions_mid.principal_id
+  description = "Principal ID used for Azure RBAC assignments."
+}
