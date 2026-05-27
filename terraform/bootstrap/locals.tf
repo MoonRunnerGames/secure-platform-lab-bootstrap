3a0edeac48ca4442ce6d517038228a20ad23f4ca
MoonRunnerGames <declan.mckenna.1994@gmail.com>
2026-05-27 18:04:49 +0100

    add mock tag

--- terraform/bootstrap/locals.tf
+++ terraform/bootstrap/locals.tf
@@ -6,6 +6,7 @@ locals {
     environment = var.environment
     managed_by  = "terraform"
     purpose     = "terraform-state"
+    test = "test tag"
   }
 
   storage_account_name = lower(replace("st${var.project_name}${var.storage_account_suffix}", "-", ""))
